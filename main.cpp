cdbc5843eb04e04571151942066f6d73d33bc454
shivamkumar6120 <nirajshivam6120@gmail.com>
2025-09-02 23:03:35 +0530

    Add imple by Switch

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@ using namespace std;
 int main()
 {
     cout<<"Calculator App";
-     
+    int num1,num2,res,choice;
+    cout<<"Enter two numbers : "<<endl;
+    cin>>num1>>num2;
+    cout<<"Enter choice : "<<endl;
+    cin>>choice;
+
+    switch(choice)
+    {
+        case 1: res = num1+num2; 
+        break;
+        default: cout<<"Invalid choice"<<endl;
+    }
     return 0;
 }
